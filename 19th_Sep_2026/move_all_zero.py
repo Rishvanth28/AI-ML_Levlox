2c923e12ff6b1893744a16c4e06252d5ee615d81
Rishvanth28 <gowririshvanth@gmail.com>
2026-09-20 09:21:00 +0530

    move all zeros to the end

---
+++ 19th_Sep_2026/move_all_zero.py
@@ -0,0 +1,7 @@
+arr = list(map(int,input("Enter array:").split()))
+for j in range (len(arr)-1):
+    for i in range(len(arr)-1-j):
+        if arr[i]<arr[i+1]:
+            arr[i],arr[i+1] = arr[i+1],arr[i]
+print(arr)
+    
