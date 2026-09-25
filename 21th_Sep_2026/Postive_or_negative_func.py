f24db9aa8cb8576d3ce251753579aeca9be469ee
Rishvanth28 <gowririshvanth@gmail.com>
2026-09-25 07:55:05 +0530

    Add script to determine if a number is positive or negative

---
+++ 21th_Sep_2026/Postive_or_negative_func.py
@@ -0,0 +1,9 @@
+def postive_negative():
+    num = int(input("Enter a number:"))
+    if num >= 0:
+        return("postive")
+    else:
+        return("negative")
+
+result = postive_negative()
+print(result)
